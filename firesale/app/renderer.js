c50ae9cc504b2f7f7ac8df93c372ab9441733c6a
xinyang3 <xinyang.zhu@outlook.com>
2020-06-06 15:48:04 +0800

    firesale

--- firesale/app/renderer.js
+++ firesale/app/renderer.js
@@ -1,3 +1,5 @@
+const { remote, ipcRenderer } = require("electron");
+const mainProcess = remote.require("./main.js");
 const marked = require("marked");
 const markdownView = document.querySelector("#markdown");
 const htmlView = document.querySelector("#html");
@@ -14,4 +16,12 @@ const renderMarkdownToHtml = (markdown) => {
 markdownView.addEventListener("keyup", (event) => {
   const currentContent = event.target.value;
   renderMarkdownToHtml(currentContent);
+});
+
+openFileButton.addEventListener("click", () => {
+  mainProcess.getFileFromUser();
+});
+ipcRenderer.on("file-opened", (event, file, content) => {
+  markdownView.value = content;
+  renderMarkdownToHtml(content);
 });
